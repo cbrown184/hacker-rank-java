1cf337e02047640e246e73d9876cdf6402227f4c
Christopher Brown <chris.brown@otcxn.com>
2018-10-11 19:54:24 +0100

    ifElse

---
+++ src/main/java/javaIfElse/Solution.java
@@ -0,0 +1,36 @@
+package main.java.javaIfElse;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    int N = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+    //If N is odd, print Weird
+    if (N%2 == 1)
+      System.out.println("Weird");
+    //If N is even and in the inclusive range of 2 to 5, print Not Weird
+    if(N%2==0 && N>=2 && N<=5)
+      System.out.println("Not Weird");
+    //If  is even and in the inclusive range of 6 to 20, print Weird
+    if(N%2==0 && N>=6 && N<=20)
+      System.out.println("Weird");
+    //If  is even and greater than 20, print Not Weird
+    if(N%2==0 && N>20)
+      System.out.println("Weird");
+
+    scanner.close();
+  }
+}
+
