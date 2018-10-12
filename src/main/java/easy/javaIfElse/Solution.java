9e3a2a17cde1ddd567e74f1e341ac710fe5cd19f
Christopher Brown <chris.brown@otcxn.com>
2018-10-12 10:40:26 +0100

    ?

--- src/main/java/easy/javaIfElse/Solution.java
+++ src/main/java/easy/javaIfElse/Solution.java
@@ -1,12 +1,6 @@
-package main.java.javaIfElse;
+package main.java.easy.javaIfElse;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class Solution {
 
